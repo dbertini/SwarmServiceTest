5cba470a28b597724a277b933e0389d25f02263b
David Bertini <david.bertini@gmail.com>
2018-11-07 14:57:44 +0100

    Prima commit

---
+++ src/main/java/it/db/swarmservice/rest/App.java
@@ -0,0 +1,17 @@
+package it.db.swarmservice.rest;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		Swarm swarm = new Swarm();
+		swarm.start();
+
+		JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+		deployment.addClass(HelloWorldResource.class);
+		swarm.deploy(deployment);
+	}
+}
---
+++ src/main/java/it/db/swarmservice/rest/AppConfig.java
@@ -0,0 +1,13 @@
+package it.db.swarmservice.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/")
+public class AppConfig extends ResourceConfig {
+
+	public AppConfig() {
+		register(HelloWorldResource.class);
+	}
+}
